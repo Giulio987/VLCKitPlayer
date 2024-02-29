f39cded0be88c386d0563ca7083026c5ec151d67
Giulio Milani <giulio.milani@develondigital.com>
2024-02-29 16:54:55 +0100

    Initial Commit

---
+++ VLCKitPlayer/VLCKitPlayer.h
@@ -0,0 +1,18 @@
+//
+//  VLCKitPlayer.h
+//  VLCKitPlayer
+//
+//  Created by Giulio Milani on 29/02/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VLCKitPlayer.
+FOUNDATION_EXPORT double VLCKitPlayerVersionNumber;
+
+//! Project version string for VLCKitPlayer.
+FOUNDATION_EXPORT const unsigned char VLCKitPlayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VLCKitPlayer/PublicHeader.h>
+
+
